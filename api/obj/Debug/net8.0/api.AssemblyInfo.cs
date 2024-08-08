fc4ee7e4f6a3a79bfd9c615c4950b6bdb8ec0e80
AaronAPerez <aaperez06@gmail.com>
2024-08-07 21:36:31 -0700

    Added ConnectionString

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e57065c03828ecf481e900305c930b766416de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67108c12e5be08b664d1ad6f452559767662e620")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
